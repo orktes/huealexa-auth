d40fd4b7b5f7d3bedddf915cb321a98c875d77d3
Jaakko Lukkari <jaakko@unity3d.com>
2017-02-22 01:41:13 +0200

    Add auth page

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -73,7 +73,36 @@ func main() {
 		w.Header().Set("content-type", "application/json")
 		w.WriteHeader(200)
 
-		io.Copy(w, resp.Body)
+		var authDetails struct {
+			AccessToken  string `json:"access_token"`
+			RefreshToken string `json:"refresh_token"`
+			ExpiresIn    int    `json:"expires_in"`
+		}
+
+		err = json.NewDecoder(resp.Body).Decode(&authDetails)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		w.Write([]byte(fmt.Sprintf(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+        <meta charset="UTF-8">
+        <title>HueAlexa Auth</title>
+        </head>
+        <body>
+          <form target="%s">
+            <input type="hidden" name="access_token" value="%s" />
+            <input type="hidden" name="refresh_token" value="%s" />
+            <input type="hidden" name="expires_in" value="%d" />
+            <input type="submit" value="Auth"/>
+          </form>
+        </body>
+      </html>
+    `, "", authDetails.AccessToken, authDetails.RefreshToken, authDetails.ExpiresIn)))
 
 	})); err != nil {
 		panic(err)
